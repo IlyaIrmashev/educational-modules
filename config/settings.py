2b8f69cf15eb34aeabdd2f3422531166f6bd2c88
Ilya Irmashev <ilya290397@gmail.com>
2024-03-17 18:57:28 +0400

    update modules

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'drf_yasg',
 
     'users.apps.UsersConfig',
+    'modules.apps.ModulesConfig',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -35,5 +35,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(('modules.urls', 'modules'), namespace='modules')),
     path('users/', include(('users.urls', 'users'), namespace='users')),
 ]
