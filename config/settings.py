4f346aa5c18c3beaa94e0f66f73bc9b80ab7504a
Ilya Irmashev <ilya290397@gmail.com>
2024-03-20 22:37:05 +0400

    completed

--- config/settings.py
+++ config/settings.py
@@ -154,3 +154,21 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1)
 }
+
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
+CELERY_TIMEZONE = "Europe/Moscow"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+    'http://10.2.3.16:8000',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'http://10.2.3.16:8000',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
